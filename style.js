31c95c97b28890ee88995e630fcb6634c71ff3b3
Dmitriy <stasyuk.dimas@gmail.com>
2023-12-04 19:25:21 +0300

    js

--- style.js
+++ style.js
@@ -1,46 +1,24 @@
 const btnUp = {
     el: document.querySelector('.btn-up'),
-    scrolling: false,
     show() {
-        if (this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
-            this.el.classList.remove('btn-up_hide');
-            this.el.classList.add('btn-up_hiding');
-            window.setTimeout(() => {
-                this.el.classList.remove('btn-up_hiding');
-            }, 300);
-        }
+        // удалим у кнопки класс btn-up_hide
+        this.el.classList.remove('btn-up_hide');
     },
     hide() {
-        if (!this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
-            this.el.classList.add('btn-up_hiding');
-            window.setTimeout(() => {
-                this.el.classList.add('btn-up_hide');
-                this.el.classList.remove('btn-up_hiding');
-            }, 300);
-        }
+        // добавим к кнопке класс btn-up_hide
+        this.el.classList.add('btn-up_hide');
     },
     addEventListener() {
-        // при прокрутке окна (window)
+        // при прокрутке содержимого страницы
         window.addEventListener('scroll', () => {
+            // определяем величину прокрутки
             const scrollY = window.scrollY || document.documentElement.scrollTop;
-            if (this.scrolling && scrollY > 0) {
-                return;
-            }
-            this.scrolling = false;
-            // если пользователь прокрутил страницу более чем на 200px
-            if (scrollY > 400) {
-                // сделаем кнопку .btn-up видимой
-                this.show();
-            } else {
-                // иначе скроем кнопку .btn-up
-                this.hide();
-            }
+            // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+            scrollY > 400 ? this.show() : this.hide();
         });
         // при нажатии на кнопку .btn-up
         document.querySelector('.btn-up').onclick = () => {
-            this.scrolling = true;
-            this.hide();
-            // переместиться в верхнюю часть страницы
+            // переместим в начало страницы
             window.scrollTo({
                 top: 0,
                 left: 0,
